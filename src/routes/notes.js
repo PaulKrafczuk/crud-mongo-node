4475a4889592679d7f8ab2488a198c2b1d894374
PaulKrfaczuk <krafczukpaul@gmail.com>
2021-08-26 12:00:37 -0300

    empezando a trabajar en las vistas de notas

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,5 +1,9 @@
 const router = require('express').Router();
 
+router.get('/notes/add', (req, res) => {
+    res.render('notes/new-note.hbs');
+});
+
 router.get('/notes', (req, res) => {
     res.send('Notes from database');
 })
