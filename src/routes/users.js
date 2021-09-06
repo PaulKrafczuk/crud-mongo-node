f7f755783e899910b85978e64c822a6221b371b2
PaulKrafczuk <krafczukpaul@gmail.com>
2021-09-06 19:55:39 -0300

    signup form validation

--- src/routes/users.js
+++ src/routes/users.js
@@ -8,5 +8,28 @@ router.get('/users/signup', (req, res) => {
     res.render('users/signup.hbs');
 });
 
+router.post('/users/signup', (req, res) => {
+    const { name, email, password, confirm_password } = req.body;
+    const errors = [];
+    if (!name) {
+        errors.push({ text: 'please insert your name' });
+    };
+
+    if (password != confirm_password) {
+        errors.push({ text: 'Password dot not match' });
+    };
+
+    if (password.length < 4) {
+        errors.push({ text: 'Password must be at least 6 characters' });
+    };
+
+    if (errors.length > 0) {
+        res.render('users/signup.hbs', { errors, name, email, password, confirm_password });
+    } else {
+        res.send('ok');
+    };
+
+});
+
 
 module.exports = router;
